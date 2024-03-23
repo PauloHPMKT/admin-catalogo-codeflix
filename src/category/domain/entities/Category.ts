ef845d070acdc1491edb9296c0a66ea7069d9a67
Paulo Sergio <pauloserg861@gmail.com>
2024-03-23 18:52:00 -0300

    feat: add entity category

--- src/category/domain/entities/Category.ts
+++ src/category/domain/entities/Category.ts
@@ -1,5 +1,26 @@
+export interface CategoryProps {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+}
+
 export class Category {
-  constructor(public name: string) {
-    this.name = name
+  constructor(private readonly props: CategoryProps) {}
+
+  get name() {
+    return this.props.name;
+  }
+
+  get desription() {
+    return this.props.description;
+  }
+
+  get is_active() {
+    return this.props.is_active;
+  }
+
+  get created_at() {
+    return this.props.created_at;
   }
 }
