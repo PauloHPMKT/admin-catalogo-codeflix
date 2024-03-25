bac1c9852dacaa90dcb6ea26aa8966d782361099
Paulo Sergio <pauloserg861@gmail.com>
2024-03-25 11:58:50 -0300

    test: ensure tests of getters and getters

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -80,3 +80,53 @@ describe('Category Entity constructor unit tests', () => {
   })
 })
 
+describe('Category Entity getters and setters unit tests', () => {
+  it('should test getter name field', () => {
+    const sut = makeSut();
+    expect(sut.name).toBe("Movies");
+  })
+
+  it('should test getter of descvription field', () => {
+    const sut = makeSut();
+    sut['props'].description = "Movies category";
+    expect(sut.desription).toBe("Movies category");
+
+    sut['props'].description = null;
+    expect(sut.desription).toBeNull();
+  })
+
+  it('should test setter of description field', () => {
+    const sut = makeSut();
+    sut['description'] = "Other description";
+    expect(sut['props'].description).toBe("Other description");
+
+    sut['description'] = undefined;
+    expect(sut['props'].description).toBeNull();
+  })
+
+  it('should test getter of is_active field', () => {
+    const sut = makeSut();
+    expect(sut.is_active).toBeTruthy();
+
+    sut['props'].is_active = false;
+    expect(sut.is_active).toBeFalsy();
+  })
+
+  it('should test setter of is_active field', () => {
+    const sut = makeSut();
+    sut['is_active'] = false;
+    expect(sut['props'].is_active).toBeFalsy();
+
+    sut['is_active'] = true;
+    expect(sut['props'].is_active).toBeTruthy();
+  })
+
+  it('should test getter of created_at field', () => {
+    const sut = makeSut();
+    let created_at = new Date();
+    sut['props'].created_at = created_at;
+
+    expect(sut.created_at).toBe(created_at);
+    expect(sut.created_at).toBeInstanceOf(Date);
+  })
+})
