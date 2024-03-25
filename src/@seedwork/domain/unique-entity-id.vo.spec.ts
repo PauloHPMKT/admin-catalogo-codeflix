6cfee87a9867182ff02b2281abb5ad6d0d66f8f2
Paulo Sergio <pauloserg861@gmail.com>
2024-03-25 16:24:10 -0300

    test: ensure validate method in value object is called and throw an error

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -4,6 +4,20 @@ import UniqueEntityId from "./unique-entity-id.vo"
 describe('UniqueEntityId unit tests', () => {
   it('sould test throw error when uuid is invalid', () => {
     expect(() => new UniqueEntityId('fake uuid'))
-      .toThrow(new InvalidUUIDError())
+      .toThrow(new InvalidUUIDError());
+  })
+
+  /** spyOn permite que eu pegue um objeto e observe uma variavel ou metodo*/
+  it('sould test if validate method was not called', () => {
+    // prototype é um objeto que contem metodos que podem ser acessados por todos os objetos de uma classe
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    expect(validateSpy).not.toHaveBeenCalled();
+  })
+
+  it('sould test if validate method was called once', () => {
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+    expect(() => new UniqueEntityId('fake id')).toThrow(new InvalidUUIDError());
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+    expect(validateSpy).toHaveBeenCalled();
   })
 })
